9c26d8b867717a26e06fdc6bb25d9523e01077b6
Felipe Velásquez Montoya <f.velasquez@uniandes.edu.co>
2019-04-02 08:18:32 -0500

    Reto 1

--- src/components/job.js
+++ src/components/job.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import {FormattedDate} from 'react-intl';
 
 export default class Job extends React.Component {
 
   	render() {
+
+      let days = (new Date() - new Date(this.props.offer.date))/86400000;
+      days = Math.trunc(days);
   		return (
   			<tr>
   				<th scope="row">{this.props.offer.id}</th>
@@ -10,7 +14,17 @@ export default class Job extends React.Component {
   				<td>{this.props.offer.company}</td>
       			<td>{this.props.offer.salary}</td>
       			<td>{this.props.offer.city}</td>
-      			<td>{this.props.offer.date}</td>
+      			<td>
+              <FormattedDate
+              value={new Date(this.props.offer.date)}
+              year='numeric'
+              month='long'
+              day='numeric'
+              weekday='long'
+              />
+
+              <div>({days} {(days > 1)? "days" : "day"} ago)</div>
+            </td>
   			</tr>
   		);
 	}
